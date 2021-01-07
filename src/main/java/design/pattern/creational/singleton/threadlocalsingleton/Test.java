34c6c64b582aeb38bcfb3ad4efc1ee2424ee5c92
李昱 <liyu@liyus-Mac-mini.local>
2021-01-07 19:07:56 +0800

    单例模式

--- src/main/java/design/pattern/creational/singleton/threadlocalsingleton/Test.java
+++ src/main/java/design/pattern/creational/singleton/threadlocalsingleton/Test.java
@@ -2,13 +2,8 @@ package design.pattern.creational.singleton.threadlocalsingleton;
 
 
 import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
-import java.util.List;
 
 public class Test {
-    private static String validString = "1234567890";
-    private static final List validStr = Arrays.asList('1', '2', '3', '4', '5', '6', '7', '8', '0');
-
     public static void main(String[] args) throws UnsupportedEncodingException {
         Thread thread1 = new Thread(new Task());
         Thread thread2 = new Thread(new Task());
